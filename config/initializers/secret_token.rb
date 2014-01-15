68dac1d689068428a9ffd8bc5d8af9adff004d90
Pablo Salgado <pablosalgadom@gmail.com>
2014-01-15 13:18:54 -0300

    Se crea el proyecto en ruby
    - Ruby 2.1.0
    - Mysql
    - Rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaginaWeb::Application.config.secret_key_base = '4b4d7021efa62900cd0870f114443212c9c1c8a37a8daa8a8dc49ca40e74c7e04830c19bd2505f3c9962960a914e168308badcc684d60b3aa28871f02e68add4'
